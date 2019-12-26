8807ed042b8473ef10b4f6e5692d08a7d31ecc84
insetindrra <57220662+insetindrra@users.noreply.github.com>
2019-12-26 22:14:28 +0700

    Create indrra.php

---
+++ indrra.php
@@ -0,0 +1,307 @@
+
+<link rel='stylesheet' id='estilo-css'  href='https://www.expertsphp.com/wp-content/plugins/simple-code-highlighter/estilo.css?ver=4.9.9' type='text/css' media='all' />
+<link rel='stylesheet' id='aquarella-style-css'  href='https://www.expertsphp.com/wp-content/themes/aquarella-lite/style.css?ver=4.9.9' type='text/css' media='all' />
+<link rel='stylesheet' id='bootstrap-css'  href='https://www.expertsphp.com/wp-content/themes/aquarella-lite/vendor/bootstrap/css/bootstrap.min.css?ver=4.9.9' type='text/css' media='all' />
+<link rel='stylesheet' id='aquarella-googlewebfonts-css'  href='https://fonts.googleapis.com/css?family=Merriweather+Sans%3A400%2C700&#038;ver=4.9.9' type='text/css' media='all' />
+<link rel='stylesheet' id='aquarella-googlewebfonts2-css'  href='https://fonts.googleapis.com/css?family=Roboto%3A400%2C700%2C300&#038;ver=4.9.9' type='text/css' media='all' />
+<link rel='stylesheet' id='aquarella-theme-style-css'  href='https://www.expertsphp.com/wp-content/themes/aquarella-lite/css/styles.css?ver=4.9.9' type='text/css' media='all' />
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<link rel='https://api.w.org/' href='https://www.expertsphp.com/wp-json/' />
+
+<link rel="icon" href="https://www.expertsphp.com/wp-content/uploads/2019/01/cropped-fev-icon-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://www.expertsphp.com/wp-content/uploads/2019/01/cropped-fev-icon-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://www.expertsphp.com/wp-content/uploads/2019/01/cropped-fev-icon-180x180.png" />
+<meta name="msapplication-TileImage" content="https://www.expertsphp.com/wp-content/uploads/2019/01/cropped-fev-icon-270x270.png" />
+<!-- Customizer Variables -->
+<!-- Customizer Styles -->
+<style>
+      .adimg{position: absolute;width: 160px}
+            .adleft{left: -70px}
+            .adright{right: -70px}
+            .adleft-fix{position: fixed;left: 20px;top: 10px;z-index: 0}
+            .adright-fix{position: fixed;right: 20px;top: 10px;z-index: 0}
+            footer{z-index:9;position:relative;}
+            @media(max-width: 767px){
+              .mnone{display: none;}
+            }
+            
+         
+         .color1,
+         a,
+         .title-of-section,
+         .btn-stroke,
+         .tags-links a,
+         .tbpw-profile-name,
+         .navigation a:hover,
+         .comment-reply-link {
+         color: #f72565;
+         }
+         .color2 {
+         color: #ffcf38;
+         }
+         .color3,
+         a:hover,
+         .sidebar-area .widget-list li a:hover,
+         .sidebar-area .widget ul li a:hover,
+         .entry-footer .cat-links a,
+         .navigation a {
+         color: #f8be27;
+         }
+         html,
+         #colophon .widget {
+         background-color: transparent;
+         }
+         body,
+         .body-boxed {
+         background-color: #fafafa;
+         }
+         .bdw-wrapper:before,
+         .bdw-wrapper:after,
+         .title-border:after,
+         .btn-default,
+         .balloon,
+         .balloon:after,
+         .dropdown-mega-content,
+         .dropdown-mega-content ul.dropdown-mega-sub-nav li a:hover,
+         .navbar-top,
+         .tagcloud a,
+         .footer-social-icons .social-icons-list a:hover,
+         .btn-special, .btn,
+         .section-title,
+         .social-icons a,
+         .navbar .nav  li  a:hover, .navbar .nav  li  a:focus, .navbar .nav  li  a:active,
+         .wpcf7-form input[type=submit], .mc4wp-form input[type=submit] {
+         background-color: #f72565;
+         }
+         .bdw-wrapper-negative:before,
+         .bdw-wrapper-negative:after,
+         .header-social-icons ul a,
+         .header-social-icons ul a:hover {
+         background-color: #ffffff;
+         }
+         .btn-default,
+         .btn-special:hover,
+         .balloon,
+         #masthead,
+         #masthead a,
+         .navbar-toggle,
+         .dropdown-mega-content,
+         #colophon,
+         #colophon a,
+         #colophon .footer-bottom,
+         #colophon .widget,
+         #colophon .widget p,
+         .section-title,
+         .section-mini-hero,
+         .sidebar-area .widget-title,
+         .sidebar-area .widget h3,
+         .entry-footer .cat-links a:hover,
+         .entry-footer .comments-link a,
+         .entry-meta,
+         .entry-meta a,
+         .comments-area input[type=submit]:hover,
+         .wpcf7-form input[type=submit] {
+         color: #ffffff;
+         }
+         body .btn-special,
+         body .btn-stroke:hover,
+         body .btn-stroke-negative,
+         body .tagcloud a,
+         body .tagcloud a:hover,
+         body .footer-social-icons .social-icons-list a,
+         body .footer-social-icons .social-icons-list a:hover,
+         body .contrast-with-image,
+         body .social-icons a,
+         body .post-element-header,
+         body .comments-area input[type=submit],
+         body .hero-slideshow {
+         color: #ffffff ;
+         }
+         body .btn-stroke {
+         border-color: #f72565;
+         }
+         body .btn-stroke:before,
+         body .navbar .nav li a,
+         body .post-element-header,
+         body .owl-carousel .owl-dot.active span {
+         background-color: #f8be27;
+         }
+         body .btn-stroke:hover {
+         border-color: #f8be27; 
+         }
+         body .btn-stroke:hover {
+         background-color: transparent;
+         }
+         body .btn-stroke-negative {
+         border-color: #ffffff ;
+         }
+         .form-control,
+         .comments-area input[type=text],
+         .comments-area input[type=email],
+         .comments-area input[type=url],
+         .comments-area textarea {
+         color: #111;
+         }
+         .navbar-toggle {
+         border-color: #ffffff;
+         }
+         .navbar-bottom,
+         .social-icons a:hover {
+         background-color: #ffcf38;
+         }
+         body .header-social-icons ul a {
+         color: #f8be27;
+         }
+         body .header-social-icons ul a:hover {
+         color: #f72565;
+         }
+         #colophon {
+         background-color: #444444;
+         }
+         .tagcloud a:hover,
+         .entry-footer .cat-links a:hover,
+         .entry-footer .comments-link a,
+         .entry-footer .comments-link a:hover,
+         .entry-meta {
+         background-color: #f8be27;
+         }
+         .contrast,
+         .contrast-box {
+         background-color: #f2f2f2;
+         }
+         .sidebar-area .widget-list li a,
+         .sidebar-area .widget ul li a,
+         .comment-metadata a {
+         color: #555;
+         }
+         body .sidebar-area .widget_search #s {
+         border-color: #ccc;
+         }
+         .sidebar-area .widget_search #s {
+         color: #fff;
+         }
+         .entry-title,
+         .entry-title a {
+         color: #4d4d4f;
+         }
+         .entry-footer .cat-links a {
+         border-color: #f8be27;
+         }
+         .comments-area input[type=text],
+         .comments-area input[type=email],
+         .comments-area input[type=url],
+         .comments-area textarea,
+         .wpcf7-form input[type=text],
+         .wpcf7-form input[type=email],
+         .wpcf7-form textarea {
+         border-color: #ffcf38;
+         }
+         .comment-author .fn a {
+         color: #555555;
+         }
+         
+         #body {
+         background-image: url(''); 
+         background-color: #eeeeee;
+         }
+</style>
+<div id="page" class="site">
+  <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+  <!-- Header
+  ================================================== -->
+  <header id="masthead" class="site-header" role="banner">
+	
+  </header>
+  <!-- /Header
+  ================================================== -->
+  <!-- /Content
+  ================================================== -->
+  <!-- == CONTENT AREA == -->
+  <div id="primary" class="content-area">
+	<div class="container">
+	  <!-- row -->
+
+
+
+	  <div class="row">
+		<!-- col -->
+		<!--ads-->
+		<div class="col-sm-1 mnone">
+		  <div class="adimg adleft">
+
+
+
+		  </div>
+		</div>
+		<div class="col-sm-10">
+		  <main id="main" class="site-main" role="main">
+			<article>
+			  <div class="post-element">
+				<!-- Featured image -->
+				<!-- / Featured-image -->
+				<header class="entry-header">
+
+				  <div class="text-center"  id="showdata">
+
+					<div class="col-sm-12">
+
+
+
+					</div>
+
+					<div class="post-has-ct"></div>
+					<h1 class="entry-title text-center">How to Save Facebook, Instagram, Twitter Videos & Images?</h1>
+
+					<form method="POST" action="/download.php">
+					  <div class="input-group" style="max-width:570px;text-align:center;margin:0 auto;">
+						<input name="url" class="form-control input-lg" placeholder="https://twitter.com/MattCaronTV/status/1119533724155224064" type="url" autofocus="" required>
+
+						<span class="input-group-btn">
+						  <button name="submit" class="btn btn-primary btn-lg">Download</button>
+						</span>
+					  </div>
+					  <div></div>
+					  <p class="text-center">Example:- <a>https://twitter.com/MattCaronTV/status/1119533724155224064</a></p>
+					</form>
+
+				  </div>
+
+
+				</header>
+				<!-- .entry-header -->
+				<div class="entry-content">
+				</div>
+				<!-- .entry-content -->
+			  </div>
+			</article>
+			<!-- #post-## -->
+		  </main>
+		</div>
+		<!-- /col -->
+
+
+		<div class="col-sm-1 mnone">
+
+		</div>
+
+	  </div>
+	  <!-- / row -->
+	</div>
+  </div>
+  <!-- == /CONTENT AREA == -->
+</div>
+
+<script type='text/javascript' src='https://www.expertsphp.com/wp-content/plugins/simple-code-highlighter/js/pretty.js?ver=4.9.9'></script>
+<script type='text/javascript' src='https://www.expertsphp.com/wp-content/themes/aquarella-lite/js/navigation.js?ver=20151215'></script>
+<script type='text/javascript' src='https://www.expertsphp.com/wp-content/themes/aquarella-lite/js/skip-link-focus-fix.js?ver=20151215'></script>
+<script type='text/javascript' src='/wp-includes/js/comment-reply.min.js?ver=4.9.9'></script>
+<script type='text/javascript' src='https://www.expertsphp.com/wp-content/themes/aquarella-lite/vendor/bootstrap/js/bootstrap.min.js?ver=20151215'></script>
+<script type='text/javascript' src='/wp-includes/js/wp-embed.min.js?ver=4.9.9'></script>
+
+
+<style>
+       .btn-block {
+           color: white;
+       }
+</style>
